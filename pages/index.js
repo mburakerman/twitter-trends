540d780528c9fe230156821399fb2e28ad0743d6
Mehmet Burak Erman <mburakerman@gmail.com>
2020-11-23 16:14:53 +0300

    changes

--- pages/index.js
+++ pages/index.js
@@ -52,7 +52,7 @@ export default function Home() {
     <div>
       <Head>
         <title>Twitter Trends</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
       </Head>
       <header>
         <h1>Twitter Trends</h1>
@@ -67,6 +67,12 @@ export default function Home() {
         })}
       */}
       </main>
+
+      <footer>
+        <ul></ul>
+        <p>Made with <a href="https://developer.twitter.com/en/docs/twitter-api" target="_blank" rel="noopener">❤️ Twitter API</a></p>
+        <p className="footer__author"><a href="https://mburakerman.github.io">Mehmet Burak Erman</a></p>
+      </footer>
     </div>
   )
 }
