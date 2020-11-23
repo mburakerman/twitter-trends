df0990de1282480efee6546c60c26e7d06ec98bf
Mehmet Burak Erman <mburakerman@gmail.com>
2020-11-23 10:17:31 +0300

    changes

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,49 @@ import Head from 'next/head'
 import Map from "./components/map.js"
 
 export default function Home() {
-  const [eventData, setEventData] = useState([])
-  const [loading, setLoading] = useState(false)
+  const [locations, setLocations] = useState([]);
+  const [countryTrends, setCountryTrends] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    async function fetchLocations() {
+      await fetch("api/locations")
+        .then(res => res.json())
+        .then(r => {
+          setLoading(false);
+          setLocations(r);
+          console.log(r);
+          var locations = r;
+          /*for (let i = 0; i < locations.length; i++) {
+            let woeid = locations[i].woeid;
+            if (woeid !== 1) {
+              fetchTrends(woeid);
+            }
+          } */
+        })
+    }
+    fetchLocations();
+    async function fetchTrends(woeid) {
+      setLoading(true)
+      await fetch("/api/trends", {
+        method: "post",
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          woeid: woeid
+        })
+      })
+        .then(res => res.json())
+        .then(function (response) {
+          setLoading(false);
+          var trends = response[0];
+          setCountryTrends(trends);
+        });
+    }
+  }, []);
 
   return (
     <div>
@@ -16,6 +57,11 @@ export default function Home() {
 
       <main>
         <Map />
+        {locations && locations.map(function (item, index) {
+          if (!item.woeid) return;
+          if (item.woeid == 1) return;
+          return <p>{item.name}</p>
+        })}
       </main>
     </div>
   )
