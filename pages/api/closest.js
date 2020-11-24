61f7730a6ab6118344cffe336da6b8b1ba32ec5b
Mehmet Burak Erman <mburakerman@gmail.com>
2020-11-24 11:30:19 +0300

    add .env and refactoring

--- pages/api/closest.js
+++ pages/api/closest.js
@@ -1,18 +1,9 @@
-var Twit = require('twit')
-
-var T = new Twit({
-    consumer_key: "n03n4khbU98MYv8SK0yWuVRJN",
-    consumer_secret: "r9M3Z3EdwedScKirSPF20EfPLZemmbBP3Q5bcW2FhxyDCjnO2g",
-    access_token: "635507198-7V97EtPDoayZuayOi8smT4338EH398RKHLzOuRvO",
-    access_token_secret: "3IFhgpZ2y1ze2WdwYTtiabxEoQENlx8IshQHvEwvRveQF",
-    timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests.
-    strictSSL: false,     // optional - requires SSL certificates to be valid.
-})
+import TWITTER from "./_twitter.js"
 
 export default async (req, res) => {
     const lat = req.body.lat;
     const lng = req.body.lng;
-    T.get('trends/closest', { lat: lat, long: lng }, function (err, data, response) {
+    TWITTER.get('trends/closest', { lat: lat, long: lng }, function (err, data, response) {
         res.status(200).json(data);
     });
 }
--- pages/api/country-flag.js
+++ pages/api/country-flag.js
@@ -1,5 +1,4 @@
 export default async (req, res) => {
     const countryCode = req.body.countryCode;
-
     res.status(200).json({ countryFlagUrl: `https://www.countryflags.io/${countryCode}/shiny/32.png` });
 }
