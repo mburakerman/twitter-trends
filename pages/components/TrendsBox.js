aea1c2da77ec12ee0c848ec359ad86cd0071225d
Mehmet Burak Erman <mburakerman@gmail.com>
2020-11-25 11:14:12 +0300

    fix build issue

--- pages/components/TrendsBox.js
+++ pages/components/TrendsBox.js
@@ -1,14 +1,14 @@
 const TrendsBox = ({ info, flag }) => {
     return (
         <div className="trends-box">
-            {info.locations &&
+            {info &&
                 <h3 className="trends-box__title">
                     {flag && <img src={flag} width="20" loading="lazy" />}{info.locations[0].name} Trends
                 </h3>
             }
             <ul className="trends-box__trends">
                 {
-                    info.trends ? info.trends.map(function (item, index) {
+                    info ? info.trends.map(function (item, index) {
                         if (!item.tweet_volume) return;
                         return <li key={index}>
                             <a href={item.url} target="_blank" rel="noopener">
