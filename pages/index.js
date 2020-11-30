2d197fae1419e7ad47e55ddfbefddc52db082c9c
Mehmet Burak Erman <mburakerman@gmail.com>
2020-11-30 11:32:52 +0300

    update footer

--- pages/index.js
+++ pages/index.js
@@ -18,9 +18,14 @@ export default function Home() {
       </main>
 
       <footer>
-        <ul></ul>
-        <p>Made with <a href="https://developer.twitter.com/en/docs/twitter-api" target="_blank" rel="noopener"><img src="./twitter.svg" width="12" /> Twitter API</a></p>
-        <p className="footer__author"><a href="https://mburakerman.github.io" target="_blank" rel="noopener">Mehmet Burak Erman</a></p>
+        <details>
+          <summary>What is this?</summary>
+          <p>Twitter Trends is an app where you click on a map and shows you the closest area's Twitter trending hashtag and topics today.</p>
+        </details>
+        <div className="footer__bottom">
+          <p>Made with <a href="https://developer.twitter.com/en/docs/twitter-api" target="_blank" rel="noopener"><img src="./twitter.svg" width="12" /> Twitter API</a></p>
+          <p><a href="https://mburakerman.github.io" target="_blank" rel="noopener">Mehmet Burak Erman</a></p>
+        </div>
       </footer>
     </div>
   )
