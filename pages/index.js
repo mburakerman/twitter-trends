a175d5c9a0a0568a90720c5b5f37c3c0bccfa7f0
Mehmet Burak Erman <mburakerman@gmail.com>
2020-11-30 16:38:33 +0300

    fix typo

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Home() {
       <footer>
         <details>
           <summary>What is this?</summary>
-          <p>Twitter Trends is an app where you click on a map and shows you the closest area's Twitter trending hashtag and topics today.</p>
+          <p>Twitter Trends is an app where you click on a map and shows you the closest area's Twitter trending topics and hashtags today.</p>
         </details>
         <div className="footer__bottom">
           <p>Made with <a href="https://developer.twitter.com/en/docs/twitter-api" target="_blank" rel="noopener"><img src="./twitter.svg" width="12" /> Twitter API</a></p>
