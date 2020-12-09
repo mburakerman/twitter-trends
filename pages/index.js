e462985777aaa61ab1ae8ba3da5862039e4d974a
Mehmet Burak Erman <mburakerman@gmail.com>
2020-12-09 18:05:21 +0300

    change footer

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ export default function Home() {
       <footer>
         <details>
           <summary>What is this?</summary>
-          <p>Twitter Trends is an app where you click on a map and shows you the closest area's Twitter trending topics and hashtags today.</p>
+          <p>Twitter Trends is an app where you click on a map and shows you the closest area's current Twitter trending topics and hashtags.</p>
         </details>
         <div className="footer__bottom">
           <p>Made with <a href="https://developer.twitter.com/en/docs/twitter-api" target="_blank" rel="noopener"><img src="./twitter.svg" width="12" /> Twitter API</a></p>
