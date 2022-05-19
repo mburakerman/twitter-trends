0a05557e0685cb5a5a80136e4765d8bbf2cba9af
mburakerman <mburakerman@gmail.com>
2022-05-19 19:02:56 +0300

    converted _app.js to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,9 @@ import '../styles/reset.css';
 import '../styles/globals.css';
 import '../styles/trends-box.css';
 import '../styles/footer.css';
+import { AppProps } from 'next/app';
 
-function App({ Component, pageProps }) {
+function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
 
