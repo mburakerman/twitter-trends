61d82d300db8242d432d4510d550c371fa251877
mburakerman <mburakerman@gmail.com>
2022-06-23 16:59:07 +0300

    implemented useQuery for trends

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,27 +1,39 @@
-import React, { FC, useState, useEffect, useContext } from 'react'
+import React, { FC, useState, useContext } from 'react'
 import GoogleMapReact from 'google-map-react'
 import MapMarker from './MapMarker'
 import TrendsBox from './TrendsBox'
 import { updateFavicon } from '../helpers/updateFavicon'
 import { GlobalContext, IClickedPosition } from '../store/index'
 import { getClosestLocation, getTrends } from '../service/index'
+import { useQuery } from 'react-query'
 
 const WOEID_WORDWIDE = 1
 interface MapProps {
-    center: IClickedPosition;
-    zoom: number;
+  center: IClickedPosition;
+  zoom: number;
 }
 
 const Map :FC<MapProps> = ({ center, zoom }) => {
   const { trendsInfo, setTrendsInfo } = useContext(GlobalContext)
   const { clickedPosition, setClickedPosition } = useContext(GlobalContext)
   const { clickedPositionCountryCode, setClickedPositionCountryCode } = useContext(GlobalContext)
-  const [loading, setLoading] = useState<boolean>(false)
   const { setTrendsBoxVisibility } = useContext(GlobalContext)
+  const [loading, setLoading] = useState<boolean>(false)
+  const [woeid, setWoeid] = useState<number>(WOEID_WORDWIDE)
+  const [countryCode, setCountryCode] = useState<string>()
 
-  useEffect(() => {
-    fetchTrends(WOEID_WORDWIDE)
-  }, [])
+  const { refetch: refechTrends } = useQuery(['trends', woeid], () => getTrends(woeid), {
+    onSuccess: (response) => {
+      setLoading(false)
+      setTrendsInfo(response[0])
+      setTrendsBoxVisibility(true)
+
+      if (woeid !== WOEID_WORDWIDE) {
+        setClickedPositionCountryCode(countryCode)
+        updateFavicon(countryCode)
+      }
+    }
+  })
 
   async function getClickedAreasWoeid (e) {
     const initialClickedPosition = {
@@ -32,47 +44,31 @@ const Map :FC<MapProps> = ({ center, zoom }) => {
 
     await getClosestLocation(initialClickedPosition)
       .then(function (res) {
-        const woeid = res[0].woeid
-        const countryCode = res[0].countryCode
-
-        fetchTrends(woeid)
-        if (woeid !== WOEID_WORDWIDE) {
-          setClickedPositionCountryCode(countryCode)
-          updateFavicon(countryCode)
-        }
-      })
-  }
-  async function fetchTrends (woeid: number) {
-    setLoading(true)
-
-    await getTrends(woeid)
-      .then(function (response) {
-        setLoading(false)
-        const data = response[0]
-        setTrendsInfo(data)
-        setTrendsBoxVisibility(true)
+        setWoeid(res[0].woeid)
+        setCountryCode(res[0].countryCode)
+        refechTrends()
       })
   }
 
   return (
-        <div id="map">
-            <GoogleMapReact
-                bootstrapURLKeys={{ key: 'AIzaSyBKcbWgVYRSdCv0PCn6dCOvgdV7MjcE-R0' }}
-                defaultCenter={ center }
-                defaultZoom={zoom}
-                options={{
-                  fullscreenControl: false
-                }}
-                onClick={(e: any) => getClickedAreasWoeid(e)}
-            >
-                {clickedPosition.lat && <MapMarker lat={clickedPosition.lat} lng={clickedPosition.lng} />}
-            </GoogleMapReact>
+      <div id="map">
+          <GoogleMapReact
+              bootstrapURLKeys={{ key: 'AIzaSyBKcbWgVYRSdCv0PCn6dCOvgdV7MjcE-R0' }}
+              defaultCenter={ center }
+              defaultZoom={zoom}
+              options={{
+                fullscreenControl: false
+              }}
+              onClick={(e: any) => getClickedAreasWoeid(e)}
+          >
+            {clickedPosition.lat && <MapMarker lat={clickedPosition.lat} lng={clickedPosition.lng} />}
+          </GoogleMapReact>
 
-            {
-                trendsInfo.locations &&
-                <TrendsBox info={trendsInfo} flag={clickedPositionCountryCode} loading={loading} />
-            }
-        </div>
+          {
+            trendsInfo.locations &&
+            <TrendsBox info={trendsInfo} flag={clickedPositionCountryCode} loading={loading} />
+          }
+      </div>
   )
 }
 
