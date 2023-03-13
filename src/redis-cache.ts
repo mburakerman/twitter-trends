1f0a81e02a1ed24be7fbdb445b3cca4034fbaf05
mburakerman <mburakerman@gmail.com>
2023-03-13 23:09:21 +0300

    implemented upstash redis for /trends api

---
+++ src/redis-cache.ts
@@ -0,0 +1,21 @@
+import redis from "./redis";
+
+export const fetchFromRedis = async <T>(
+  key: string,
+  fetchFunction: () => Promise<T>,
+  expireTime: number = 3600
+): Promise<T> => {
+  try {
+    const redisData = await redis.get(key);
+    if (redisData) {
+      return JSON.parse(redisData);
+    }
+
+    const data = await fetchFunction();
+    await redis.set(key, JSON.stringify(data), "EX", expireTime);
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
