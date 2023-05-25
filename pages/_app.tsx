c0a39caa2088a83f4a58b6ef2961adce959ccf26
Mehmet Burak Erman <mburakerman@gmail.com>
2023-05-25 20:22:52 +0300

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import "../src/styles/global.css";
+import "../src/global.css";
 import { AppProps } from "next/app";
 import React from "react";
 import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
